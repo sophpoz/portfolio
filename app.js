344b9e773722b9bf23585019c8cf2be6c8a1cdd5
Sophia Pozzi <sopozzi@gmail.com>
2016-11-22 09:31:00 -0800

    nav and scroll work

--- app.js
+++ app.js
@@ -1,25 +1,36 @@
 
 $(document).ready(function(){ 
 
-            var scrollPix = 0;
-             
-            $(document).scroll(function() { 
-            	console.log(window.scrollY)  
-                scrollPix = $(this).scrollTop();
-                if(scrollPix > 700 && scrollPix < 1530) {
-                   $('.nAbout').css('color','#333745');
-                   $('.nWork').css('color','#333745');
-                   $('.nContact').css('color','#333745');
-                }
-                else {
-                    $('.nAbout').css('color','#FE5F55');
-                    $('.nWork').css('color','#FE5F55');
-                    $('.nContact').css('color','#FE5F55');
-                }
-            });
-            //  $("#contactCreator").click(function(){
-            // $("#contactCreatorText").fadeIn(1000);
-            // });
+  var scrollPix = 0;
+
+  $(document).scroll(function() { 
+   console.log(window.scrollY)  
+   scrollPix = $(this).scrollTop();
+   if(scrollPix > 700 && scrollPix < 1530) {
+     $('.nAbout').css('color','#333745');
+     $('.nWork').css('color','#333745');
+     $('.nContact').css('color','#333745');
+   }
+   else {
+    $('.nAbout').css('color','#FE5F55');
+    $('.nWork').css('color','#FE5F55');
+    $('.nContact').css('color','#FE5F55');
+  }
+});
+  $(document).ready(function(){
+    $('a[href^="#"]').on('click',function (e) {
+      e.preventDefault();
+
+      var target = this.hash;
+      var $target = $(target);
+
+      $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 900, 'swing', function () {
+        window.location.hash = target;
+      });
+    });
+  });
 });
 $(document).ready(function(){
     $("#contactCreator").click(function(){
@@ -39,5 +50,11 @@ $(document).ready(function(){
     });
     $("a.close").click(function(){
       $("#yogaQuizText").fadeOut(1000);
+    });
+    $("#etsyButton").click(function(){
+      $("#etsyText").fadeIn(1000);
+    });
+    $("a.close").click(function(){
+      $("#etsyText").fadeOut(1000);
     });
 });
