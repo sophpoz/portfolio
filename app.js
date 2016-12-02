fb907f4e128f7848f919ca19f133f30bc8a7ab3b
Sophia Pozzi <sopozzi@gmail.com>
2016-12-02 07:21:18 -0800

    about, button and scroll updates

--- app.js
+++ app.js
@@ -12,31 +12,40 @@ $(document).ready(function(){
         window.location.hash = target;
       });
     });
+
 });
 $(document).ready(function(){
     $("#contactCreator").click(function(){
       $("#contactCreatorText").fadeIn(1000);
+      $("body").css('overflow', 'hidden');
     });
     $("a.close").click(function(){
       $("#contactCreatorText").fadeOut(1000);
+      $("body").css('overflow', 'auto')
     });
     $("#hotCold").click(function(){
       $("#hotColdText").fadeIn(1000);
+      $("body").css('overflow', 'hidden');
     });
     $("a.close").click(function(){
       $("#hotColdText").fadeOut(1000);
+      $("body").css('overflow', 'auto')
     });
     $("#yogaQuiz").click(function(){
       $("#yogaQuizText").fadeIn(1000);
+      $("body").css('overflow', 'hidden');
     });
     $("a.close").click(function(){
       $("#yogaQuizText").fadeOut(1000);
+      $("body").css('overflow', 'auto')
     });
     $("#etsyButton").click(function(){
       $("#etsyText").fadeIn(1000);
+      $("body").css('overflow', 'hidden');
     });
     $("a.close").click(function(){
       $("#etsyText").fadeOut(1000);
+      $("body").css('overflow', 'auto')
     });
     //sp home
     $(window).scroll(function() {
